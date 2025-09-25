1c28e03615a37d6175a7fdc1f06172206fb7ca19
RoandyellDagoy <roandyellkhendagoy@gmail.com>
2025-09-25 23:47:30 +0800

    backend api tested

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -4,14 +4,21 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Services\GeminiServices;
 use App\Http\Controllers\GeminiController;
+use App\Http\Controllers\AuthController;
 
 
+Route::post('/register',[AuthController::class, 'register']);
+Route::post('/login',[AuthController::class, 'login']);
+Route::post('/logout', [AuthController::class, 'logout']);
 
-
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::middleware('auth:sanctum')->group(function (){
+    Route::get('/me',[AuthController::class, 'profile']);
+    Route::post('/logout',[AuthController::class, 'logout']);   
+});
 
 Route::post('/gemini/generate', [GeminiController::class, 'generate']);
 Route::post('/gemini/chat', [GeminiController::class, 'chat']);
+
+
+
 
